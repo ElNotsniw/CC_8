c04efbffdf8113f3c75f16e55350501601c173b2
ElNotsniw <winstonjosh010604@gmail.com>
2025-02-13 10:20:49 -0500

    Task 3 - Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -31,3 +31,33 @@ let calculatediscount = function(price, discountrate) {
 
 console.log(`Final Price: $${calculatediscount(100, 0.2)}`)                     // Output: Final Price: $80
 console.log(`Final Price: $${calculatediscount(250, 0.15)}`)                    // Output: Final Price: $212.5
+
+
+
+
+
+// Task 3 - Arrow Function (Scenario: Service Fee Calculation)
+
+// Setting up an arrow function to calculate a fee
+
+let calculateservicefee = (amount, servicetype) => {
+    let feepercentage;
+
+    switch (servicetype) {
+        case "Premium":
+            feepercentage = 0.15;
+            break;
+        case "Standard":
+            feepercentage = 0.10;
+            break;
+        case "Basic":
+            feepercentage = 0.05
+            break
+    }
+
+    return amount * feepercentage
+};
+
+
+console.log(`Service Fee: $${calculateservicefee(200, "Premium")}`)                     // Output: Service Fee: $30
+console.log(`Service Fee: $${calculateservicefee(500, "Standard")}`)                    // Output: Service Fee: $50
